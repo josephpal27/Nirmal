b184e3e568dcb6d8f3340997fee9cf414f747639
Joseph Pal <josephpal27@gmail.com>
2025-01-20 17:07:16 +0530

    Add responsive product toggle functionality and styles for mobile view

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -27,3 +27,45 @@ document.querySelectorAll('.mobile-view .journey-btns button').forEach((btn, ind
         btn.classList.add('active');
     });
 });
+
+
+// --------------------------------------------------------------------------------------------------------------------- //
+
+// Functionality For Product page Toggle
+let typeBtns = document.querySelectorAll('.products-by-type-btn-row button');
+let typeBoxes = document.querySelectorAll('.products-by-type-boxes .product-box');
+
+// Add event listener to each button
+typeBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        // Remove 'active' class from all buttons
+        typeBtns.forEach(button => button.classList.remove('active'));
+        // Add 'active' class to the clicked button
+        btn.classList.add('active');
+
+        // Hide all product boxes
+        typeBoxes.forEach(box => box.classList.add('hide'));
+        // Show the relevant product box
+        typeBoxes[index].classList.remove('hide');
+    });
+});
+
+
+
+let industryBtns = document.querySelectorAll('.products-by-industry-btn-row button');
+let industryBoxes = document.querySelectorAll('.products-by-industry-boxes .product-box');
+
+// Add event listener to each button
+industryBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        // Remove 'active' class from all buttons
+        industryBtns.forEach(button => button.classList.remove('active'));
+        // Add 'active' class to the clicked button
+        btn.classList.add('active');
+
+        // Hide all product boxes
+        industryBoxes.forEach(box => box.classList.add('hide'));
+        // Show the relevant product box
+        industryBoxes[index].classList.remove('hide');
+    });
+});
